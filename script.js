297834088ef02f22013b50e00ad8f7fc80e0b4cb
Tenseroid <tenseroid@gmail.com>
2025-08-09 15:56:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// Subtle glitch text effect
+// Glitch effect
 const glitchText = document.querySelector('.glitch');
 setInterval(() => {
   glitchText.style.textShadow =
     Math.random() > 0.5
       ? '2px 0 red, -2px 0 blue'
       : '-2px 0 red, 2px 0 blue';
-}, 200);
+}, 150);
